2b6f8eefe90c8549025fa31382081015a4fa0fbb
atul-6272 <atulaman6272@gmail.com>
2016-09-29 17:59:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/article-one', function (req, res) {
+    res.send('article one is served and it will be requested')
+});
+app.get('/article-one', function (req, res) {
+    res.send('article one is served and it will be requested')
+});
+app.get('/article-one', function (req, res) {
+    res.send('article one is served and it will be requested')
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
